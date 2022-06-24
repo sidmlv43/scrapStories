46ae06f5de50781b402253957f90d82860864096
Siddharth <sidmlv43@gmail.com>
2022-06-25 05:24:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,94 @@
+const http = require("http");
+const https = require("https");
+
+const fs = require("fs");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    https.get("https://time.com/", (response) => {
+      let data = [];
+
+      response.on("data", (chunk) => {
+        data.push(chunk);
+      });
+      response.on("end", () => {
+        fs.writeFileSync("time.html", data.join("").toString(), function (err) {
+          if (err) {
+            console.log(err);
+          }
+          console.log("file written");
+        });
+
+        fs.readFile("time.html", (err, data) => {
+          if (err) {
+            console.log(err);
+          }
+          const parsedData = data.toString();
+
+          const arrayData = parsedData.split("</div>");
+
+          const targetDiv = arrayData.filter((el) =>
+            el.includes(`class="latest-stories__item"`)
+          );
+          const targetDivString = targetDiv.toString();
+          const targetList = targetDivString.split("</li>");
+          const targetLink = targetList.map((el) => {
+            const link = el.match(/<a[^>]*href=["']([^"']*)["']/g);
+            return link;
+          });
+
+          //   console.log(targetList);
+
+          const realLink = targetLink.map((link) => {
+            return String(link);
+          });
+
+          const extactedLink = realLink.map((link) => {
+            return link.substring(9, link.length - 1);
+          });
+          //   console.log(extactedLink);
+
+          const postTitleTags = targetList.map((el) =>
+            el.match(/<h3\sclass="latest-stories__item-headline">(.*)<\/h3>/)
+          );
+
+          console.log(postTitleTags);
+          const postTitleTagsString = postTitleTags.toString();
+          const processedPostTitleString = postTitleTagsString.split(",");
+
+          const extractedPostTitleString = [
+            processedPostTitleString[1],
+            processedPostTitleString[3],
+            processedPostTitleString[5],
+            processedPostTitleString[7],
+            processedPostTitleString[9],
+            processedPostTitleString[11],
+          ];
+
+          const resData = [];
+
+          console.log(extractedPostTitleString);
+          for (const key in extractedPostTitleString) {
+            console.log(key);
+            resData.push({
+              title: extractedPostTitleString[key],
+              link: "https://www.time.com/" + extactedLink[key],
+            });
+          }
+
+          console.log(resData);
+
+          res.writeHead(200, {
+            "Content-type": "application/json",
+          });
+          res.end(JSON.stringify({ data: resData }));
+        });
+      });
+      //   console.log(data);
+    });
+  }
+});
+
+server.listen(3000, (req, res) => {
+  console.log("server is running at http://localhost:3000");
+});
---
+++ test.js
@@ -0,0 +1,96 @@
+const lst = [
+    [
+      `<h3 class="latest-stories__item-headline">Reactions to the End of 'Roe' Reveal Divisions Between Religious Leaders</h3>`,
+      "Reactions to the End of 'Roe' Reveal Divisions Between Religious Leaders",
+      index: 290,
+      input: '\n' +
+        '        <div class="partial latest-stories" data-module_name="Latest Stories">\n' +
+        '          <h2 class="latest-stories__heading">Latest Stories</h2>\n' +
+        '          <ul>\n' +
+        '            <li class="latest-stories__item">\n' +
+        '              <a href="/6191048/religious-abortion-roe-reaction/">\n' +
+        `                <h3 class="latest-stories__item-headline">Reactions to the End of 'Roe' Reveal Divisions Between Religious Leaders</h3>\n` +
+        '              </a>\n' +
+        '              <time class="latest-stories__item-timestamp">\n' +
+        '                June 24, 2022 • 7:12 PM EDT\n' +
+        '              </time>\n' +
+        '            ',
+      groups: undefined
+    ],
+    [
+      '<h3 class="latest-stories__item-headline">Hackers Steal $100 Million by Exploiting Crypto’s Weak Link</h3>',
+      'Hackers Steal $100 Million by Exploiting Crypto’s Weak Link',
+      index: 131,
+      input: '\n' +
+        '            <li class="latest-stories__item">\n' +
+        '              <a href="/6190924/hackers-exploit-crypto-weak-link/">\n' +
+        '                <h3 class="latest-stories__item-headline">Hackers Steal $100 Million by Exploiting Crypto’s Weak Link</h3>\n' +
+        '              </a>\n' +
+        '              <time class="latest-stories__item-timestamp">\n' +
+        '                June 24, 2022 • 7:06 PM EDT\n' +
+        '              </time>\n' +
+        '            ',
+      groups: undefined
+    ],
+    [
+      `<h3 class="latest-stories__item-headline">Filmmaker Rebeca Huntt's Debut Documentary <i>BEBA</i> Is an Intimate Self-Portrait</h3>`,
+      "Filmmaker Rebeca Huntt's Debut Documentary <i>BEBA</i> Is an Intimate Self-Portrait",
+      index: 126,
+      input: '\n' +
+        '            <li class="latest-stories__item">\n' +
+        '              <a href="/6191173/rebeca-huntt-beba-interview/">\n' +
+        `                <h3 class="latest-stories__item-headline">Filmmaker Rebeca Huntt's Debut Documentary <i>BEBA</i> Is an Intimate Self-Portrait</h3>\n` +
+        '              </a>\n' +
+        '              <time class="latest-stories__item-timestamp">\n' +
+        '                June 24, 2022 • 6:08 PM EDT\n' +
+        '              </time>\n' +
+        '            ',
+      groups: undefined
+    ],
+    [
+      '<h3 class="latest-stories__item-headline">Women Athletes Worry After Roe v. Wade Falls</h3>',
+      'Women Athletes Worry After Roe v. Wade Falls',
+      index: 136,
+      input: '\n' +
+        '            <li class="latest-stories__item">\n' +
+        '              <a href="/6190819/women-athletes-roe-abortion-reactions/">\n' +
+        '                <h3 class="latest-stories__item-headline">Women Athletes Worry After Roe v. Wade Falls</h3>\n' +
+        '              </a>\n' +
+        '              <time class="latest-stories__item-timestamp">\n' +
+        '                June 24, 2022 • 5:46 PM EDT\n' +
+        '              </time>\n' +
+        '            ',
+      groups: undefined
+    ],
+    [
+      '<h3 class="latest-stories__item-headline">Juul Wins Emergency Order Blocking FDA Ban</h3>',
+      'Juul Wins Emergency Order Blocking FDA Ban',
+      index: 136,
+      input: '\n' +
+        '            <li class="latest-stories__item">\n' +
+        '              <a href="/6191272/juul-emergency-order-blocking-fda-ban/">\n' +
+        '                <h3 class="latest-stories__item-headline">Juul Wins Emergency Order Blocking FDA Ban</h3>\n' +
+        '              </a>\n' +
+        '              <time class="latest-stories__item-timestamp">\n' +
+        '                June 24, 2022 • 5:42 PM EDT\n' +
+        '              </time>\n' +
+        '            ',
+      groups: undefined
+    ],
+    [
+      '<h3 class="latest-stories__item-headline">What to Know About Abortion Funds and How They Help Patients Get Care</h3>',
+      'What to Know About Abortion Funds and How They Help Patients Get Care',
+      index: 122,
+      input: '\n' +
+        '            <li class="latest-stories__item">\n' +
+        '              <a href="/6190996/how-abortion-funds-work/">\n' +
+        '                <h3 class="latest-stories__item-headline">What to Know About Abortion Funds and How They Help Patients Get Care</h3>\n' +
+        '              </a>\n' +
+        '              <time class="latest-stories__item-timestamp">\n' +
+        '                June 24, 2022 • 5:23 PM EDT\n' +
+        '              </time>\n' +
+        '            ',
+      groups: undefined
+    ],
+    null
+  ]
